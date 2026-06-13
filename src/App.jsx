c8bfe423d3a1a72e17c3d0ad84dfcc84090ed089
uy <a.normurzaev20@gmail.com>
2026-06-14 00:56:47 +0500

    navbar css stil

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-
-import React from 'react'
-import Navbar from './Campanents/Navbar/Navbar'
-
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'; 
+import Navbar from './Campanents/Navbar/Navbar';
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-    </div>
-  )
-}
+    <BrowserRouter> 
+      <div>
+        <Navbar />
+       </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
