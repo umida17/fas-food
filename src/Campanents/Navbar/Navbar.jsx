ed89b327d54ac15e2ad3e142020e20dbdca3f54f
uy <a.normurzaev20@gmail.com>
2026-06-14 00:44:58 +0500

    navbar css

--- src/Campanents/Navbar/Navbar.jsx
+++ src/Campanents/Navbar/Navbar.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
-import Navbar from '../Navbar.css'
-import frame from '../../assets/Frame 64(1).png'
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import './Navbar.css'; 
+import logo from '../../assets/Frame 64(1).png';
 
 const Navbar = () => {
-  return (
-    <div>
-      <div className="header">
-        <div className="container">
-            <div className="header-container">
-                <img src={frame} alt="" />
-                <ul>
-                    <li><a href="#">Menu</a></li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                    <li><a href="#">Log in</a></li>
-                </ul>
-            </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+  // Navigatsiya linklarini massiv ko'rinishida yozsak, kodimiz toza va oson kengayadigan bo'ladi
+  const navLinks = [
+    { path: '/menu', label: 'Menu' },
+    { path: '/news', label: 'News' },
+    { path: '/about', label: 'About Us' },
+    { path: '/contact', label: 'Contact Us' },
+  ];
 
-export default Navbar
+  return (
+    <header className="header">
+      <div className="container header-container">
+         <Link to="/" className="logo-link">
+          <img src={logo} alt="Eat Turkish Logo" className="logo-img" />
+        </Link>
+
+         <nav className="nav-menu">
+          <ul className="nav-list">
+            {navLinks.map((link) => (
+              <li key={link.path} className="nav-item">
+                <NavLink 
+                  to={link.path} 
+                  className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+                >
+                  {link.label}
+                </NavLink>
+              </li>
+            ))}
+            
+             <li className="nav-item">
+              <Link to="/login" className="login-btn">
+                Log in
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
