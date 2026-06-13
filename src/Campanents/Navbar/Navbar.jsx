d8a3d9524164a4f1c7cc511e554ef1a5ce526657
uy <a.normurzaev20@gmail.com>
2026-06-14 00:39:35 +0500

    navbar

--- src/Campanents/Navbar/Navbar.jsx
+++ src/Campanents/Navbar/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Navbar from '../Navbar.css'
-
+import frame from '../../assets/Frame 64(1).png'
 
 const Navbar = () => {
   return (
     <div>
-      
+      <div className="header">
+        <div className="container">
+            <div className="header-container">
+                <img src={frame} alt="" />
+                <ul>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Log in</a></li>
+                </ul>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
