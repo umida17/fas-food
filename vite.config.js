7cc409e61f683e7072532f25b3579c770cbef229
uy <a.normurzaev20@gmail.com>
2026-06-16 21:02:46 +0500

    davom etamiz

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-plugin'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/' // Mana shu qator borligiga ishonch hosil qiling
 })
